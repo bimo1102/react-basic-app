7ca99696e9c2102db3d57e523b6ef8705b7ab486
lamlvhe140728 <lamlvhe140728@fpt.edu.vn>
2023-12-04 23:03:18 +0700

    12/04/2023 11:03pm

--- src/views/Example/Home.js
+++ src/views/Example/Home.js
@@ -1,7 +1,14 @@
 import React from 'react'
-import { withRouter } from 'react-router-dom/cjs/react-router-dom.min';
+// import { withRouter } from 'react-router';
+import Color from '../HOC/Color';
+
 
 class Home extends React.Component {
+    componentDidMount() {
+        setTimeout(() => {
+            this.props.history.push('/todos')
+        }, 3000)
+    }
     render() {
         return (
             <div>
@@ -10,4 +17,5 @@ class Home extends React.Component {
         )
     }
 }
-export default withRouter(Home);
+export default Color(Home);
+// export default withRouter(Home);
